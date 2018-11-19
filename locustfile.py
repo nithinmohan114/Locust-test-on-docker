ea2e23bc8a74b501fc1665a37e169edd294a8027
Nithin <nithinmohan114@gmail.com.com>
2018-11-19 12:42:47 +0530

    Add locust test

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,23 @@
 from locust import HttpLocust, TaskSet, task
 
+
 class HomescreenTest(TaskSet):
 
     @task
     def home(self):
         self.client.get("/")
 
+    @task
+    def polls(self):
+        self.client.get("/polls")
+
+    @task
+    def polls_list(self):
+        self.client.get("/polls/2/")
+
+
 class HomeTest(HttpLocust):
     task_set = HomescreenTest
     # minimum and maximum wait in milli seconds
     min_wait = 1000
     max_wait = 5000
-
