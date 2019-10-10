3c692fe49ffe09e985354017c43eab05c9b55723
Nithin Mohan <nithinmohan114@gmail.com>
2019-10-10 16:09:45 +0530

    Add sequential order in test order

--- locustfile.py
+++ locustfile.py
@@ -3,15 +3,15 @@ from locust import HttpLocust, TaskSet, task
 
 class HomescreenTest(TaskSet):
 
-    @task
+    @task(1)
     def home(self):
         self.client.get("/")
 
-    @task
+    @task(2)
     def polls(self):
         self.client.get("/polls")
 
-    @task
+    @task(3)
     def polls_list(self):
         self.client.get("/polls/2/")
 
@@ -19,5 +19,5 @@ class HomescreenTest(TaskSet):
 class HomeTest(HttpLocust):
     task_set = HomescreenTest
     # minimum and maximum wait in milli seconds
-    min_wait = 1000
-    max_wait = 5000
+    min_wait = 5
+    max_wait = 4
